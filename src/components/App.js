5437b5de8cf97b877ece6cb211d4add0ee85656a
William Buck <wsbuckiii@gmail.com>
2019-11-29 21:58:27 -0800

    Add redux and sematic-ui and organize folders

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Upload</button>
+    </div>
+  );
+}
+
+export default App;
