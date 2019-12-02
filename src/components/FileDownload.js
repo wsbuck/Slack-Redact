b338511b8fa8729d5110c8d8b26c2689e51d28b0
William Buck <wsbuckiii@gmail.com>
2019-12-01 16:04:53 -0800

    unflatten json when downloading

--- src/components/FileDownload.js
+++ src/components/FileDownload.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+
+import flatten from 'flat';
 
 import Button from '@material-ui/core/Button';
 
 const FileDownload = () => {
   const dataObj = useSelector(state => state.editJSON.data);
   const filename = useSelector(state => state.editJSON.name);
-  const dataStr = (
-    "data:text/json;charset=utf-8," + 
-    encodeURIComponent(JSON.stringify(dataObj))
-  );
+  const [dataStr, setDataStr] = useState('');
+
+  // const dataStr = (
+  //   "data:text/json;charset=utf-8," + 
+  //   encodeURIComponent(JSON.stringify(dataObj))
+  // );
+
+  useEffect(() => {
+    const tempObj = dataObj.map((item) => {
+      return flatten.unflatten(item);
+    });
+    setDataStr(
+      'data:text/json;charset=utf-8,' + 
+      encodeURIComponent(JSON.stringify(tempObj))
+    );
+
+  }, [dataObj]);
 
   return (
     <Button variant="outlined" href={dataStr} download={filename}>
