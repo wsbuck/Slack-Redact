ea4cb94c0a31e4cbd03fde214cdc8d5f93b03eea
William Buck <wsbuckiii@gmail.com>
2019-12-04 21:05:30 -0800

    Add edit buttons
    * Add "redact-all" button
    * Add edit button

--- src/components/EditCard.js
+++ src/components/EditCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -7,6 +7,11 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import EditIcon from '@material-ui/icons/Edit';
 
 import { updateField } from '../redux/actions';
 
@@ -31,16 +36,51 @@ const useStyles = makeStyles(theme => ({
     marginLeft: 'auto',
     width: '90%',
   },
+  iconButton: {
+    padding: 5,
+  },
+  divider: {
+    height: 20,
+    margin: 4,
+  },
+  inputRoot: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    // width: 500,
+    width: '100%',
+  },
 }));
 
 const EditCard = ({ data }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const fileName = useSelector(state => state.editJSON.name);
+  const [disabled, setDisabled] = useState({});
 
   function handleChange(key, value) {
     dispatch(updateField(key, value));
   }
+
+  function handleRedact(key) {
+    dispatch(updateField(key, "XXXXX"));
+  }
+
+  function handleEdit(key) {
+    let temp = Object.assign({}, disabled, {
+      [key]: !disabled[key]
+    });
+    setDisabled(temp);
+  }
+
+
+  useEffect(() => {
+    const tempDisabled = {};
+    Object.keys(data).forEach((key) => {
+      tempDisabled[key] = true;
+    });
+    setDisabled(tempDisabled);
+  }, [data])
 
   return (
     <Card className={classes.card}>
@@ -51,20 +91,37 @@ const EditCard = ({ data }) => {
         <form className={classes.container} noValidate autoComplete="off">
           {
             Object.keys(data).map((key, index) => (
-              <TextField
-                key={index}
-                label={key}
-                value={data[key]}
-                onChange={(e) => handleChange(key, e.target.value)}
-                // disabled
-                multiline
-                variant="outlined"
-                className={
-                  key.match(/\./)
-                  ? classes.subTextField
-                  : classes.textField
-                }
-              />
+              <div className={classes.inputRoot} key={index}>
+                <TextField
+                  label={key}
+                  value={data[key]}
+                  onChange={(e) => handleChange(key, e.target.value)}
+                  multiline
+                  className={classes.textField}
+                  inputProps={{ 'aria-label': key }}
+                  variant="outlined"
+                  disabled={disabled[key]}
+                />
+                <IconButton
+                  className={classes.iconButton}
+                  aria-label='redact all'
+                  onClick={() => handleRedact(key)}
+                >
+                  <HighlightOffIcon />
+                </IconButton>
+                <Divider
+                  className={classes.divider}
+                  orientation="vertical"
+                />
+                <IconButton
+                  className={classes.iconButton}
+                  aria-label="directions"
+                  onClick={() => handleEdit(key)}
+                >
+                  <EditIcon />
+                </IconButton>
+              </div>
+                
             ))
           }
         </form>
